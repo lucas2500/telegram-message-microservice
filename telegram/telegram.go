751447f501a9f8cd1d40327e6b7a2767ab9738b4
lucas2500 <lucas.barbosa@grupoelfa.com.br>
2022-05-21 22:43:48 -0300

    feat: adicionado parâmetro para envio de botões
    Agora é possível enviar botões na mensagem.

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -12,6 +12,7 @@ import (
 func SendMessageToTelegram(body []byte) {
 
 	var message entity.Message
+	var buttons []entity.Buttons
 
 	err := json.Unmarshal(body, &message)
 
@@ -19,7 +20,20 @@ func SendMessageToTelegram(body []byte) {
 		log.Fatal(err, "Erro no parsing do json!!")
 	}
 
-	req, err := http.Get(os.Getenv("TELEGRAM_BASE_URL") + message.BotToken + "/" + os.Getenv("TELEGRAM_ROUTE") + "?chat_id=" + message.ChatId + "&text=" + message.Text + "&parse_mode=" + message.ParseMode)
+	for i := range message.InlineKeyboard {
+		button := entity.Buttons{{Text: message.InlineKeyboard[i].Text, CallbackData: message.InlineKeyboard[i].CallbackData}}
+		buttons = append(buttons, button)
+	}
+
+	ReplyMarkup := entity.ReplyMarkup{Buttons: buttons}
+
+	InlineKeyBoard, err := json.Marshal(&ReplyMarkup)
+
+	if err != nil {
+		log.Fatal(err, "Erro ao marshalar o json!!")
+	}
+
+	req, err := http.Get(os.Getenv("TELEGRAM_BASE_URL") + message.BotToken + "/" + os.Getenv("TELEGRAM_ROUTE") + "?chat_id=" + message.ChatId + "&text=" + message.Text + "&parse_mode=" + message.ParseMode + "&reply_markup=" + string(InlineKeyBoard))
 
 	if err != nil {
 		log.Fatal(err, "Erro ao enviar mensagem ao Telegram!!")
