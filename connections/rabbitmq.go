9e1ef6c3137eff76a0733a1502e5b27a28c817c7
lucasbarbosa01 <33992359+lucas2500@users.noreply.github.com>
2022-09-23 21:49:15 -0300

    feat: melhorando função de conexão
    10 tentativas de conexões serão feitas caso em casos de falha

--- connections/rabbitmq.go
+++ connections/rabbitmq.go
@@ -1,6 +1,7 @@
 package connections
 
 import (
+	"fmt"
 	"os"
 	"telegram-message-microservice/util"
 
@@ -9,8 +10,26 @@ import (
 
 func ConnectToRabbitMQ() *amqp.Connection {
 
-	dsn := "amqp://" + os.Getenv("RABBITMQ_DEFAULT_USER") + ":" + os.Getenv("RABBITMQ_DEFAULT_PASS") + "@" + os.Getenv("RABBITMQ_DEFAULT_HOST") + ":" + os.Getenv("RABBITMQ_DEFAULT_PORT") + os.Getenv("RABBITMQ_DEFAULT_VHOST")
-	conn, err := amqp.Dial(dsn)
+	var (
+		counter int = 0
+		dsn     string
+		conn    *amqp.Connection
+		err     error
+	)
+
+	for {
+
+		counter++
+
+		fmt.Println("Tentativa", counter, "de conexão!!")
+
+		dsn = "amqp://" + os.Getenv("RABBITMQ_DEFAULT_USER") + ":" + os.Getenv("RABBITMQ_DEFAULT_PASS") + "@" + os.Getenv("RABBITMQ_DEFAULT_HOST") + ":" + os.Getenv("RABBITMQ_DEFAULT_PORT") + os.Getenv("RABBITMQ_DEFAULT_VHOST")
+		conn, err = amqp.Dial(dsn)
+
+		if err == nil || counter == 10 {
+			break
+		}
+	}
 
 	if err != nil {
 		util.FailOnError(err, "Falha ao se conectar ao RBMQ")
